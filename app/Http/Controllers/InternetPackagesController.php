2d1af921c62ca3d10b9cf29a6eeefbe8c45db2de
basantasharma <gyawalibasanta0@gmail.com>
2024-04-01 14:14:33 +0545

    add internet package controller added

--- app/Http/Controllers/InternetPackagesController.php
+++ app/Http/Controllers/InternetPackagesController.php
@@ -11,26 +11,36 @@ class InternetPackagesController extends Controller
     //
     public function addInternetPackage(Request $request)
     {
+        $request['created_by'] = auth()->user()->username;
+        $request['updated_by'] = auth()->user()->username;
         if($request['service_isactive'])
         {
             $request['service_isactive'] = true;
         }
+        else
+        {
+            $request['service_isactive'] = false;
+        }
         $request->validate([
             'service_name' => 'required|string|max:50',
-            'service_isactive' => 'required|boolean|max:50',
-            'service_price' => 'required|integer|max:50',
-            'service_duration' => 'required|number|min:1|max:12',
-            'service_upload_bandwidth' => 'required|integer|max:50',
-            'service_download_bandwidth' => 'required|integer|max:50',
-            'service_limit_daily' => 'required|integer|max:50',
-            'service_limit_monthly' => 'required|integer|max:50',
-            'service_limit_scope' => 'required|integer|max:50',
+            'service_isactive' => 'required|boolean',
+            'service_price' => 'required|integer',
+            'service_duration' => 'required|integer|min:1|max:12',
+            'service_upload_bandwidth' => 'required|integer',
+            'service_download_bandwidth' => 'required|integer',
+            'service_limit_daily' => 'required|integer',
+            'service_limit_monthly' => 'required|integer',
+            'service_limit_scope' => 'required|integer',
             'service_description' => 'required|string|max:250',
         ]);
         $internetPackage = InternetPackages::create($request->all());
         if($internetPackage)
         {
             return redirect()->back()->with('success', 'package added successfully');
+        }
+        else
+        {
+            return redirect()->back()->with('failed', 'sorry data packages cannot be added')->withInput();
         }
         dd($request);
     }
--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -30,14 +30,128 @@
       </div>
 
       @role('admin')
+      <div class="row text-start mb-4">
+        <div class="col">
+            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#addinternetpackage">
+                Add Internet Package
+              </button>
+        </div>
+      </div>
+
+      <form class="needs-validation" novalidate action="/addinternetpackage" method="post">
+        <div class="modal modal-lg" id="addinternetpackage">
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+        
+              <!-- Modal Header -->
+              <div class="modal-header bg-dark text-light">
+                <h5 class="modal-title">Add Internet Package</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+              </div>
       
+              <!-- Modal body -->
+              <div class="modal-body">
+                @csrf
+                @method('post')
+                <div class="serviceform">
+                    <div class="row justify-content-start">
+                        {{-- <h3 class="mb-1 text-center">Internet Package</h3> --}}
+                        <h4 class="text-center">Internet Package</h4><hr>
+                
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control"  id="service_name" name="service_name" placeholder="Service Name">
+                            <label for="service_name">Service Name</label>
+                          </div>
+                        </div>
+            
+                        
+
+                        <div class="col-12 col-xxl-4 col-xl-4 col-lg-4 col-md-6 col-sm-6 mb-3">
+                          <label for="service_isactive">Service Enabled:</label>
+                          <div class="form-switch fs-4">
+                            <input type="checkbox" id="service_isactive" class="form-check-input" role="switch" name="service_isactive" checked>
+                          </div>
+                        </div>
+
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="number" class="form-control" id="service_duration" name="service_duration" placeholder="Service Duration">
+                            <label for="service_duration">Service Duration</label>
+                          </div>
+                        </div>
+                        
+                                   
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_price" name="service_price" placeholder="Service Price">
+                            <label for="service_price">Service Price</label>
+                          </div>
+                        </div>
+            
+            
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_upload_bandwidth" name="service_upload_bandwidth" placeholder="Service Upload Bandwidth">
+                            <label for="service_upload_bandwidth">Service Upload Bandwidth</label>
+                          </div>
+                        </div>
+            
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_download_bandwidth" name="service_download_bandwidth" placeholder="Service Download Bandwidth">
+                            <label for="service_download_bandwidth">Service Download Bandwidth</label>
+                          </div>
+                        </div>
+            
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_limit_daily" name="service_limit_daily" placeholder="Service Limit/day">
+                            <label for="service_limit_daily">Service Limit/day</label>
+                          </div>
+                        </div>
+            
+                        
+                          
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_limit_monthly" name="service_limit_monthly" placeholder="Service Limit/month">
+                            <label for="service_limit_monthly">Service Limit/month</label>
+                          </div>
+                        </div>
+            
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_limit_scope" name="service_limit_scope" placeholder="Service Limit Scope">
+                            <label for="service_limit_scope">Service Limit Scope</label>
+                          </div>
+                        </div>
+            
+                        <div class="col-12 col-xxl-6 col-xl-4 col-lg-6 col-md-6 col-sm-6 mb-3">
+                          <div class="form-floating">
+                            <input type="text" class="form-control" id="service_description" name="service_description" placeholder="Service Description">
+                            <label for="service_description">Service Description</label>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="modal-footer">
+                      <button type="submit" class="btn btn-outline-dark">Submit</button>
+                      <button type="button" class="btn btn-outline-danger" data-bs-dismiss="modal">Cancle</button>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+
       @endrole
 
-    @role('manager')
+    {{-- @role('manager')
       <section class="vh-60" style="background-color: #508bfc;">
         <div class="container-fluid py-3 h-60">
           <div class="row d-flex justify-content-center align-items-center h-60">
-              <!-- @permission('editor') -->
+              @permission('editor')
               <div class="col-12 col-md-6 col-lg-4 col-xl-2 m-2">
                 <div class="card shadow-2-strong" style="border-radius: 1rem;">
                   <div class="card-body p-4 text-left">
@@ -55,8 +169,8 @@
                   </div>
                 </div>
               </div>
-              <!-- @endpermission -->
-              <!-- @permission('editor') -->
+              @endpermission
+              @permission('editor')
               <div class="col-12 col-md-6 col-lg-4 col-xl-3 m-2">
                 <div class="card shadow-2-strong" style="border-radius: 1rem;">
                   <div class="card-body p-4 text-left">
@@ -79,7 +193,7 @@
                   </div>
                 </div>
               </div>
-              <!-- @endpermission -->
+              @endpermission
               <div class="col-12 col-md-6 col-lg-4 col-xl-3 m-2">
                 <div class="card shadow-2-strong" style="border-radius: 1rem;">
                   <div class="card-body p-4 text-left">
@@ -262,6 +376,6 @@
           </div>
         </div>
       </section>
-    @endrole
+    @endrole --}}
   @endauth
 @endsection
