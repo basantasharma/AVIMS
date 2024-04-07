83f19d5bea9ac1154cdbf9dc5d180378eb9cb71d
basantasharma <gyawalibasanta0@gmail.com>
2024-04-07 17:28:24 +0545

    nas controllerchanged nas model changed radius database configured

--- app/Models/RadCheck.php
+++ app/Models/RadCheck.php
@@ -5,21 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Nas extends Model
+class RadCheck extends Model
 {
     use HasFactory;
-    protected $table = 'nas';
+    protected $table = 'radcheck';
     protected $connection = 'radius';
     
     public $timestamps = false;
     protected $fillable = [
-        'nasname',
-        'shortname',
-        'type',
-        'ports',
-        'secret',
-        'server',
-        'community',
-        'description',
+        'username',
+        'attribute',
+        'op',
+        'value'
     ];
 }
