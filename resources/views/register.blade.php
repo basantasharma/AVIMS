a8fff9130209d84a456438358ce1cb43c16487b2
ishan004 <84827925+ishan004@users.noreply.github.com>
2024-04-25 15:28:02 +0545

    ....

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -95,7 +95,7 @@
               <div class="form-floating">
                 <select class="form-control" id="internet_package" name="internet_package">
                   @foreach($availableInternetPackages as $package)
-                    <option value="{{ $package->id }}">{{ $package->service_download_bandwidth }} Mbps for {{ $package->service_duration }} months</option>
+                    <option value="{{ $package->id }}"{{ (old('internet_package') == $package->id) ? 'selected' : '' }} >{{ $package->service_download_bandwidth }} Mbps for {{ $package->service_duration }} months</option>
                   @endforeach
                 </select>
                 <label for="internet_package">Select Internet Package</label>
@@ -105,9 +105,9 @@
             <div class="col-12 col-xxl-6 col-xl-4 col-lg-4 col-md-6 col-sm-6 mb-3">
               <div class="form-floating">
                 <select class="form-control" id="iptv_package" name="iptv_package">
-                  <option value="">None</option>
+                  <option value="" selected>None</option>
                   @foreach($availableIptvPackages as $package)
-                    <option value="{{ $package->id }}">{{ (int)$package->no_of_HD_channels + (int)$package->no_of_SD_channels }} Channels for {{ $package->service_duration }} months</option>
+                    <option value="{{ $package->id }}" {{ (old('iptv_package') == $package->id) ? 'selected' : '' }}>{{ (int)$package->no_of_HD_channels + (int)$package->no_of_SD_channels }} Channels for {{ $package->service_duration }} months</option>
                   @endforeach
                 </select>
                 <label for="internet_package">Select Iptv Package</label>
